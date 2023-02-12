47c0059e9260724479c5b7ee72b9028cac005857
Nela <nr20180374@student.fon.bg.ac.rs>
2023-02-12 12:21:15 +0100

    napravljena prva verzija pocetne stranice

---
+++ footer.php
@@ -0,0 +1,28 @@
+</body>
+<footer id="footer" class="footer">
+  <div class="container text-center">
+
+  <h3>Možete nas zapratiti i na društvenim mrežama</h3>
+  <ul class="social-links">
+  <!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<div id="container">
+<div id="main">
+
+</div>
+</div>
+<li><a href="#link"><i class="fa fa-facebook"></i></a></li>
+<li><a href="#link"><i class="fa fa-instagram"></i></a></li>
+<li><a href="#link"><i class="fa fa-youtube"></i></a></li>
+
+ 
+</body>
+</html>
+</ul>
+ Picerija Picamanija, 2022
+  </div>
+</footer>
---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Picamanija</title>
+    <link rel="icon" href="../Domaci-PHP/img/logo.jpg">
+    <meta name="description" content="">
+    <meta name="keywords" content="">
+
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Open+Sans|Candal|Alegreya+Sans">
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/imagehover.min.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/slike.css">
+
+
+  </head>
+  <body>
+    <?php include 'header.php'; ?>
+
+
+    <section id ="feature" class="section-padding">
+      <div class="container">
+        <div class="row">
+          <div class="header-section text-center">
+           
+            <div class="col-md-13 text-center">
+              <img src="img/logo1.jpg" class="img img-square"> <br><br>
+            </div>
+           <p>" Picerija Picamanija " otvorena 2003. godine, danas je jedna od najposećenijih picerija na Banovom Brdu. 
+             U autentičnom, zanimljivom i toplom ambijentu, u kome je svaki detalj posebno osmišljen i unikatno ručno izrađen, 
+             pruža vam poseban doživljaj i ako samo svratite da popijete kafu. Picamanija je tu da postane nezaobilazni deo dana.
+             Picamanija je ušuškana u jedan od najlepših delova Banovog Brda, u ulici Požeška 41. Pokrenuta je kao porodični
+             biznis koji predstavlja udružena znanja i iskustva iz oblasti ugostiteljstva, dizajna i marketinga porodice Bogićević.
+</p>
+<hr class="bottom-line">
+  
+  <div class="col-md-12 text-center">
+    <div class="row">
+        <div class="column">
+            <img src="img/ambijent.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+            <img src="img/pica.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+            <img src="img/pravi.jpg"  style="width:100%">
+        </div>
+        <div class="column">
+            <img src="img/slatkaPica.jpg"  style="width:100%">
+        </div>
+    </div>
+    
+  </div>
+  
+  
+            
+
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <?php include 'footer.php'; ?>
+
+
+
+    
+ 
+  </body>
+</html>
